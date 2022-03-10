6cc556bcfc933377425fa02692ad8d2f0fa724ed
H-Yuuki <yuukirinnma@gmail.com>
2022-03-10 21:56:09 +0900

    add: プロトタイププロジェクトを新規作成

---
+++ Prototype/TIMELAB-Prototype/TIMELAB-Prototype/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TIMELAB-Prototype
+//
+//  Created by 濵田　悠樹 on 2022/03/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

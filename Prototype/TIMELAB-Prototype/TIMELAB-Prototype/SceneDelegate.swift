08c558ded9cc2ddf9e7c1bb169dadd9a3ee57b86
H-Yuuki <yuukirinnma@gmail.com>
2022-03-11 20:51:00 +0900

    add: storyboardなしで画面描画を行えるよう変更を加えた, アプリ起動時の画面呼び出しは SceneDelegate で行っている Firebase を使う時 AppDelegate を使うから

--- Prototype/TIMELAB-Prototype/TIMELAB-Prototype/SceneDelegate.swift
+++ Prototype/TIMELAB-Prototype/TIMELAB-Prototype/SceneDelegate.swift
@@ -13,10 +13,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        // アプリ起動時に表示する画面の描画
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        self.window = window
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {

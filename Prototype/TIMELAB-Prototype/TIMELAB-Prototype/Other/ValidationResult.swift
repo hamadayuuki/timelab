f0bca760584ee5a3fd062387b8fb2f9078129b92
H-Yuuki <yuukirinnma@gmail.com>
2022-03-18 18:20:18 +0900

    add: 研究室登録用の VM を作成, 登録時に使用データの状態を管理する enum を作成

---
+++ Prototype/TIMELAB-Prototype/TIMELAB-Prototype/Other/ValidationResult.swift
@@ -0,0 +1,48 @@
+//
+//  ValidationResult.swift
+//  TIMELAB-Prototype
+//
+//  Created by 濵田　悠樹 on 2022/03/18.
+//
+
+import UIKit
+
+// データの状態(テキスト と 文字色)
+enum ValidationResult {
+    case ok(message: String)
+    case empty(message: String)
+    case validating
+    case failed(message: String)
+
+    var description: String {
+        switch self {
+        case let .ok(message) : return message
+        case let .empty(message) : return message
+        case .validating : return ""
+        case let .failed(message) : return message
+        }
+    }
+
+    var isValid: Bool {
+        switch self {
+        case .ok:
+            return true
+        default:
+            return false
+        }
+    }
+    
+    var testColor: UIColor {
+        switch self {
+        case .ok:
+            return UIColor.green
+        case .empty:
+            return UIColor.black
+        case .validating:
+            return UIColor.black
+        case .failed:
+            return UIColor.red
+        }
+    }
+}
+

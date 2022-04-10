b0de6cffb4187642cabe6ad5e56ef5e2d4039933
H-Yuuki <yuukirinnma@gmail.com>
2022-04-10 13:40:03 +0900

    add: ボタンのアニメーションを追加

--- Release/TIMELAB/TIMELAB/View/RegisterUserView/RegisterButton.swift
+++ Release/TIMELAB/TIMELAB/View/RegisterUserView/RegisterButton.swift
@@ -24,6 +24,23 @@ class RegisterButton: UIButton {
         }
     }
     
+    override var isSelected: Bool {
+        didSet {
+            if isSelected {
+                // ボタンのアニメーション
+                UIView.animate(withDuration: 0.2, delay: 0, options: .curveEaseIn, animations: {
+                    self.transform = CGAffineTransform(scaleX: 1.08, y: 1.08)
+                }) { (_) in
+                    //             ↓ アニメーション時間  ↓ 待機時間  ↓ バネのような動き 振幅の大きさ(0~1)   ↓ 初速                 ↓ 追加機能
+                    UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 10, options: .curveEaseOut, animations: {
+                        self.transform = .identity
+                        
+                    }, completion: nil)
+                }
+            }
+        }
+    }
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }

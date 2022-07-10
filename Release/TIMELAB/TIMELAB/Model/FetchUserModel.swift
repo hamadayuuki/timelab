8ca411b6e22f1d2f651862239c98e257c5ac50d2
H-Yuuki <yuukirinnma@gmail.com>
2022-07-10 19:01:53 +0900

    add: ログインしているユーザーのuid取得

--- Release/TIMELAB/TIMELAB/Model/FetchUserModel.swift
+++ Release/TIMELAB/TIMELAB/Model/FetchUserModel.swift
@@ -5,4 +5,27 @@
 //  Created by 濵田　悠樹 on 2022/07/10.
 //
 
-import Foundation
+import Firebase
+import FirebaseFirestore
+import FirebaseAuth
+import RxSwift
+import RxCocoa
+import UIKit
+
+class FetchUserModel {
+    
+    func fetchUserId() -> Observable<String> {
+        
+        return Observable<String>.create { observer in
+            
+            if let user = Auth.auth().currentUser  {
+                observer.onNext(user.uid)
+            }
+            observer.onNext("")
+            
+            return Disposables.create { print("Observable: Dispose") }
+        }
+       
+    }
+    
+}

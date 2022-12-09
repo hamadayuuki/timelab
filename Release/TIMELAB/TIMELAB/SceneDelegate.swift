3aef91c7615166c77186b702a7e42b504a7fb8d2
H-Yuuki <yuukirinnma@gmail.com>
2022-12-10 03:33:10 +0900

    fix: 画面遷移

--- Release/TIMELAB/TIMELAB/SceneDelegate.swift
+++ Release/TIMELAB/TIMELAB/SceneDelegate.swift
@@ -19,17 +19,19 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // アプリ起動時に表示する画面の描画
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        // push遷移
+        // 画面遷移
         let window = UIWindow(windowScene: windowScene)
         self.window = window
         window.makeKeyAndVisible()
-        window.rootViewController = TabBarViewController()
         
-//        if let user = Auth.auth().currentUser  {
-//            window.rootViewController = TabBarViewController()   // 起動時の画面遷移
-//        } else {
-//            window.rootViewController = ChooseRegisterOrLogInViewController()   // 起動時の画面遷移
-//        }
+        if let user = Auth.auth().currentUser  {
+            window.rootViewController = TabBarViewController()   // 起動時の画面遷移
+        } else {
+            // Push通知, 遷移先でPresent遷移を可能にするため
+            let chooseRegisterOrLogInViewController = ChooseRegisterOrLogInViewController()   // 起動時の画面遷移
+            let navigationController = UINavigationController(rootViewController: chooseRegisterOrLogInViewController)
+            window.rootViewController = navigationController
+        }
         
         // Present遷移
 //       let window = UIWindow(windowScene: windowScene)
